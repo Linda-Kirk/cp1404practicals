1d84d87f2e86301a210b17b30be0e6dd303bf20f
Linda-Kirk <linda.kirk@my.jcu.edu.au>
2019-08-25 19:21:59 +1000

    Add random number generator to quick_pick program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,4 +7,16 @@ These values should be stored as CONSTANTS.
 
 import random
 
+QUANTITY_NUMBERS_PER_LINE = 6
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+
 number_quick_picks = input('Enter the number of quick picks required: ')
+numbers = []
+# for quick_pick in range(number_quick_picks):
+
+for i in range(QUANTITY_NUMBERS_PER_LINE):
+    number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+    numbers.append(number)
+print(numbers)
+
