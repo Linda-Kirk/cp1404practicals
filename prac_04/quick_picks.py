a639c756762315d028ebfcdc1048880f0d57a0f2
Linda-Kirk <linda.kirk@my.jcu.edu.au>
2019-08-25 18:56:52 +1000

    Initial commit

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,10 @@
+"""
+This program will generate "quick picks". The number of quick picks required
+will be obtained from user. The program will then generate that many lines of
+output. Each line will consist of 6 random numbers between 1 and 45.
+These values should be stored as CONSTANTS.
+"""
+
+import random
+
+number_quick_picks = input('Enter the number of quick picks required: ')
