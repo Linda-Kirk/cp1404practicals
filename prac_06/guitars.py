c52a797e13ed18cada41e467917c8d20f5eafe1e
Linda-Kirk <linda.kirk@my.jcu.edu.au>
2019-09-13 15:35:34 +1000

    Add print statement prior to printing all guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -18,6 +18,7 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+    print('These are my guitars:')
     for i, guitar in enumerate(guitars):
         vintage_string = ' (Vintage)' if guitar.is_vintage() else ''
         print("Guitar {}: {:20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.make, guitar.model, guitar.year,
