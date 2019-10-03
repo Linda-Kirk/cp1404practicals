438d01a007dd3752d1754f1bdaab89eb82a77d56
Linda-Kirk <linda.kirk@my.jcu.edu.au>
2019-10-03 15:40:06 +1000

    Create SilverServiceTaxi class which is a subset of Taxi class

---
+++ prac_08/silver_service_taxi.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 Practical
+SilverServiceTaxi class
+"""
+from prac_08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness (scale) and flagfall."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness=0.0):
+        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return "plus ${:.2f}".format(super().__str__(), self.flagfall)
