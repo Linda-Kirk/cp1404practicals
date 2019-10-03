37d32111682c68d5a6f8eee1c0a7f1bcb6fc05a4
Linda-Kirk <linda.kirk@my.jcu.edu.au>
2019-10-03 13:41:19 +1000

    Remove reference to price_per_km in testing code

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -6,8 +6,7 @@ from prac_08.taxi import Taxi
 def main():
     name = "Prius 1"
     fuel = 100
-    price_per_km = 1.23
-    taxi_1 = Taxi(name, fuel, price_per_km)
+    taxi_1 = Taxi(name, fuel)
     print(taxi_1)
 
     taxi_1.drive(40)
