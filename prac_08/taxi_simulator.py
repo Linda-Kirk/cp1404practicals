7b4d74f5e665c087a5c8cad852c7d8c74e2ca1a2
Linda-Kirk <linda.kirk@my.jcu.edu.au>
2019-10-08 10:09:06 +1000

    Refactor program to use drive_taxi function and use constants for menu and prompt

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -3,58 +3,66 @@
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
+PROMPT = ">>>"
+
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Mercedes", 75, 1.5), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
 
     bill_to_date = 0
-    current_taxi = None     # TODO: Find out how this is supposed to work!
+    current_taxi = None
 
     print("Let's drive!")
-    print("q)uit, c)hoose taxi, d)rive")
-    menu_choice = input('>>>').upper()
+    print(MENU)
+    menu_choice = input(PROMPT).upper()
     while menu_choice != 'Q':
         if menu_choice == 'C':
-            print("Taxis available:")
-            display_taxis(taxis)
-            taxi_choice = get_taxi_choice(taxis)
-            current_taxi = taxis[taxi_choice]
+            current_taxi = get_taxi_choice(taxis)
         elif menu_choice == 'D':
-            try:
-                current_taxi.start_fare()
-                distance = int(input("Drive how far? "))
-                current_taxi.drive(distance)
-                trip_cost = current_taxi.get_fare()
-                print("Your {} trip cost you ${:.2f}".format(current_taxi.name, trip_cost))
-                bill_to_date += trip_cost
-            except AttributeError:
+            if current_taxi is not None:
+                bill_to_date += drive_taxi(current_taxi)
+            else:
                 print("A taxi must be chosen before it can be driven")
         else:
-            print('Invalid menu choice')
+            print("Invalid menu choice")
         print("Bill to date: ${:.2f}".format(bill_to_date))
-        print("q)uit, c)hoose taxi, d)rive")
-        menu_choice = input('>>>').upper()
+        print(MENU)
+        menu_choice = input(PROMPT).upper()
 
     print("Total trip cost: ${:.2f}".format(bill_to_date))
     print("Taxis are now:")
     display_taxis(taxis)
 
 
+def drive_taxi(current_taxi):
+    current_taxi.start_fare()
+    distance = int(input("Drive how far? "))
+    current_taxi.drive(distance)
+    trip_cost = current_taxi.get_fare()
+    print("Your {} trip cost you ${:.2f}".format(current_taxi.name, trip_cost))
+    return trip_cost
+
+
 def get_taxi_choice(taxis):
+    taxi_choice = 0
+    print("Taxis available:")
+    display_taxis(taxis)
+
     valid_taxi_number = False
     while not valid_taxi_number:
         try:
             taxi_choice = int(input("Choose taxi: "))
             if taxi_choice < 0:
-                print('Number must be >= 0')
+                print("Number must be >= 0")
             elif taxi_choice > len(taxis) - 1:
-                print('Invalid taxi choice number')
+                print("Invalid taxi choice number")
             else:
                 valid_taxi_number = True
         except ValueError:
-            print('Invalid input; enter a valid number')
-    return taxi_choice
+            print("Invalid input; enter a valid number")
+    return taxis[taxi_choice]
 
 
 def display_taxis(taxis):
